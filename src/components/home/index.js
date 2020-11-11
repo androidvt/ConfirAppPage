0a001649910238637986ea2009d2da6f28ada031
androidvt@virtualtec.com <devios@DevIOSs-MacBook-Pro.local>
2020-11-11 09:38:29 -0500

    zIndexCorrect

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Jumbotron from './jumbotron';
+import './index.css';
 
 export default () => {
     return (
-        <div>
-            <Jumbotron />
+        <div className="jumbo border rounded">
+ <div className="row">
+                <div className="s float-right text-center  offset-md-6">
+                    <div className="jumbotron-header">ConfirmApp</div>
+                    <div className="row">
+                        <div className=" offset-md-2">
+                         Con<b> Confirmapp </b> podrás gestionar todos tus eventos de manera fácil e intuitiva
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
